883c2b30bb8bdfe9227f444a8fcee1dcfa988e3a
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-11 15:39:46 -0300

    Changed the return of get.

--- src/model/repository/CrudRepository.ts
+++ src/model/repository/CrudRepository.ts
@@ -1,5 +1,5 @@
 export default interface CrudRepository<T> {
-    get(page: number): T[];
+    get(page: number): {data: T[], pages: number, total: number};
     save(entity: T): T;
     update(entity: T): T;
     delete(entity: T): T;
