a04e6edbd7458f7a491e8f8bd54d4a5feab604db
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-11 16:26:59 -0300

    Added the function to check if poll already exists.

--- src/model/repository/PollRepository.ts
+++ src/model/repository/PollRepository.ts
@@ -2,5 +2,5 @@ import Poll from "../entity/Poll";
 import CrudRepository from "./CrudRepository";
 
 export default interface PollRepository extends CrudRepository<Poll> {
-    
+    pollAlreadyExists(poll: Poll): Promise<boolean>;
 }
