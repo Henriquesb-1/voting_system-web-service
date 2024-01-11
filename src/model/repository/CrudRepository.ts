45c0690edb750bfc30b21e8677c8c3d4dfffa7f1
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-11 15:41:36 -0300

    Changed the repository to return a Promise.

--- src/model/repository/CrudRepository.ts
+++ src/model/repository/CrudRepository.ts
@@ -1,6 +1,6 @@
 export default interface CrudRepository<T> {
-    get(page: number): {data: T[], pages: number, total: number};
-    save(entity: T): T;
-    update(entity: T): T;
-    delete(entity: T): T;
+    get(page: number): Promise<{data: T[], pages: number, total: number}>;
+    save(entity: T): Promise<T>;
+    update(entity: T): Promise<T>;
+    delete(entity: T): Promise<T>;
 }
